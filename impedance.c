22b80ad4e03c8410f7006024eb7e1f34174cc186
Thomas Court <tcourt@zynex.com>
2024-01-20 12:03:56 -0600

    Fixes for tune view and ported to Windows gcc

--- impedance.c
+++ impedance.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include <complex.h>
 #include <assert.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/ioctl.h>
-#include <termios.h>
+extern int _getch();
+
+// Fow Windows only, to allow console to use ANSI, add the following to the Registry
+// HKEY_CURRENT_USER\Console\ add the DWORD VirtualTerminalLevel to equal 1 
+
+// For PC comment out the following section:
+// For Mac or Linux, uncomment out this following section:
+// #include <unistd.h>
+// #include <stdlib.h>
+// #include <sys/ioctl.h>
+// #include <termios.h>
+// int _getch(void) {
+//     char chbuf;
+//     int nread;
+//     struct termios oldstate, newstate;
+//     tcgetattr(0, &oldstate);
+//     newstate = oldstate;
+//     newstate.c_lflag &= ~ICANON;
+//     newstate.c_lflag &= ~ECHO;
+//     tcsetattr(0, TCSANOW, &newstate);
+//     do {
+//         nread = read(0, &chbuf, 1);
+//     } while (nread <= 0);
+//     tcsetattr(0, TCSANOW, &oldstate);
+//     return chbuf;
+// }
+
 
 #define ANSI_CLEAR_SCREEN_AND_HOME "\033[2J\033[H"
+// #define ANSI_HIGHLIGHT "\033[30;47m"         // gray background
+#define ANSI_HIGHLIGHT "\033[7m"             // reverse video
+#define ANSI_NORMAL "\033[0m"
 
 #define N_RESISTANCES sizeof(resistances)/sizeof(double)
 #define N_REACTANCES sizeof(reactances)/sizeof(double)
@@ -34,24 +61,6 @@ double tunedMapSWR[2][256][256];
 char tunedMapHit[2][256][256]; 
 
 
-int _getch(void) {
-    char chbuf;
-    int nread;
-    struct termios oldstate, newstate;
-
-    tcgetattr(0, &oldstate);
-    newstate = oldstate;
-    newstate.c_lflag &= ~ICANON;
-    newstate.c_lflag &= ~ECHO;
-    tcsetattr(0, TCSANOW, &newstate);
-    do {
-        nread = read(0, &chbuf, 1);
-    } while (nread <= 0);
-    tcsetattr(0, TCSANOW, &oldstate);
-    return chbuf;
-}
-
-
 int calc_capacitors(int values)
 {
     int c[] = {22, 47, 100, 220, 470, 1000, 2220};      // ATU-10 C array in parralel, pF
@@ -74,17 +83,10 @@ double calc_inductors(int values)
 }
 
 
-double complex imp2smith(double complex imp)
-{
-    return (imp - 50.0) / (imp + 50.0);
-    // inverse if needed is -50*(s+1)/(s-1)
-}
-
-
 double calcSWR(double complex imp)
 {
-    double smith = cabs(imp2smith(imp));
-    double n = (1.0+smith) / (1.0-smith);
+    double gamma = cabs((imp - 50.0) / (imp + 50.0));
+    double n = (1.0+gamma) / (1.0-gamma);
     if (n != n)
         n = INFINITY;
     return n;
@@ -121,7 +123,7 @@ double complex ZhpLsd(double freq, double complex Zin, int L, int C)
 }
 
 
-double exaustiveSearch(double freq, complex double Zin)
+double exaustiveSearch(double freq, double complex Zin)
 {
     double smallest = INFINITY;
     for (int linx=0; linx<sizeof(inductors)/sizeof(int); linx++) {
@@ -141,10 +143,12 @@ double exaustiveSearch(double freq, complex double Zin)
 
 
 extern int SWR;
+extern char ind, cap, SW;
 double complex tuneImp = 50.0;
-int tuneFreqInx = 0;
+double tuneFreq = 0;
 double SWRexact = 1.0;
 int tuneCount;
+int lastTuneC, lastTuneL, lastTuneCsw;
 
 void delay_ms(int ms)
 {
@@ -165,30 +169,45 @@ void get_SWR()
 void Relay_set(char l, char c, char i)
 {
     if (i) // TODO - which side should the C be on the LC?, guessing for now, probably doesn't matter
-        SWRexact = calcSWR(ZhpLsu(freqs[tuneFreqInx], tuneImp, inductors[l], capacitors[c ^ 0x7f]));
+        SWRexact = calcSWR(ZhpLsu(tuneFreq, tuneImp, inductors[l], capacitors[c ^ 0x7f]));
     else
-        SWRexact = calcSWR(ZhpLsd(freqs[tuneFreqInx], tuneImp, inductors[l], capacitors[c ^ 0x7f]));
+        SWRexact = calcSWR(ZhpLsd(tuneFreq, tuneImp, inductors[l], capacitors[c ^ 0x7f]));
     assert(SWRexact >= 1.0);
     tuneCount++;
     tunedMapHit[i][l][c]++;
+    lastTuneC = c;
+    lastTuneL = l;
+    lastTuneCsw = i;
 }
 
 
-void viewLCTuneMap(int tuneChoice, double freq, complex double Zin)
+void resetTune(double freq, double complex Zin)
+{
+    tuneFreq = freq;
+    tuneImp = Zin;
+    tuneCount = 0;
+    ind = 0;
+    cap = 0;
+    SW = 0;
+    SWR = 999;
+    SWRexact = 10;
+}
+
+void viewLCTuneMap(int tuneChoice, double freq, double complex Zin)
 {
     // increase xsize and ysize to view a larger terminal view //
     const int xsize = 64, ysize = 32;
     const int lcsize = 256;
-    int xoff = 0, yoff = 0, capsw = 0, sinx = 0, zoom = 4, hit = 0;
+    int xoff = 0, yoff = 0, capsw = 0, sinx = 0, zoom = 4;
 
     for (int linx=0; linx<lcsize; linx++) {
         for (int cinx=0; cinx<lcsize; cinx++) {
-            tunedMapSWR[0][linx][cinx] = calcSWR(ZhpLsu(freq, Zin, inductors[linx], capacitors[cinx]));
-            tunedMapSWR[1][linx][cinx] = calcSWR(ZhpLsd(freq, Zin, inductors[linx], capacitors[cinx]));
+            tunedMapSWR[0][linx][cinx] = calcSWR(ZhpLsd(freq, Zin, inductors[linx], capacitors[cinx ^ 0x7f]));
+            tunedMapSWR[1][linx][cinx] = calcSWR(ZhpLsu(freq, Zin, inductors[linx], capacitors[cinx ^ 0x7f]));
         }
     }
-    
-    tuneCount = 0;
+
+    resetTune(freq, Zin);  
     memset(tunedMapHit, 0, sizeof(tunedMapHit));
     extern void tune1();
     extern void tune2();
@@ -200,52 +219,57 @@ void viewLCTuneMap(int tuneChoice, double freq, complex double Zin)
     for (;;) {
         double curScale = scale[sinx];
         printf(ANSI_CLEAR_SCREEN_AND_HOME); 
-        printf("y=%d, x=%d, cSw=%d, zoom=%d, hit=%d, %.1f+%.1fj\n", yoff, xoff, capsw, zoom, hit, creal(Zin), cimag(Zin));
+        printf("y=%d, x=%d, cSw=%d, zoom=%d   %.1f%+.1fj   tunes=%d   #=%.2f SWR\n", yoff, xoff, capsw, zoom, creal(Zin), cimag(Zin), tuneCount, SWR/100.0);
         for (int y=yoff, r=0; y<yoff+ysize*zoom; y+=zoom, r++) {
             for (int x=xoff; x<xoff+xsize*zoom; x+=zoom) {
                 // find lowest value in zoom x zoom square
                 double swr = 1000;
-                int neverHit = 1;
+                int wasHit = 0;
+                int wasPrime = 0;
                 for (int y1=y; y1<y+zoom; y1++) 
                     for (int x1=x; x1<x+zoom; x1++) {
                         double t = tunedMapSWR[capsw][y1][x1];
-                        if (hit && tunedMapHit[capsw][y1][x1] == 0)
-                            continue;
                         if (t < swr)
                             swr = t;
-                        neverHit = 0;
+                        if (tunedMapHit[capsw][y1][x1] != 0)
+                            wasHit = 1;
+                        if (lastTuneC == x1 && lastTuneL == y1 && lastTuneCsw == capsw)
+                            wasPrime = 1;
                     }
-                if (swr < -1000 || (hit && neverHit))
+                if (swr < -1000)
                     swr = -1000;
                 if (swr > 1000)
                     swr = 1000;   
                 swr = swr - 1.0;
                 int n = (int)floor(swr / curScale);
-                printf("%c", (n < 0) ? '-' : ((n < 10) ? ('0' + n) : '+'));
+                if (wasHit)
+                    printf(ANSI_HIGHLIGHT);
+                printf("%c", (wasPrime) ? '#' : ((n < 10) ? ('0' + n) : '+'));
+                if (wasHit)
+                    printf(ANSI_NORMAL);
             }
             const char *format = "  %c =%c%4.1f%c";
-            if (r == 0)
-                printf("  - =never hit");
-            else if (r < 11)
-                printf(format, '0'+r-1, ' ', 1.0+curScale*(r-1), '+');
-            else if (r == 11)
-                printf(format, '+', ' ', 1.0+curScale*(r-1), '+');
+            if (r < 10)
+                printf(format, '0'+r, ' ', 1.0+curScale*r, '+');
+            else if (r == 10)
+                printf(format, '+', ' ', 1.0+curScale*r, '+');
 
             switch(r)
             {
-            case 13:    printf("  SCROLL");         break;
-            case 14:    printf("    i");            break;
-            case 15:    printf("  j + k");          break;
-            case 16:    printf("    m");            break;
-            case 18:    printf("  PAGE");           break;
-            case 19:    printf("    I");            break;
-            case 20:    printf("  J + K");          break;
-            case 21:    printf("    M");            break;
-            case 23:    printf("  c)ap switch");    break;
-            case 24:    printf("  h)its");          break;
-            case 25:    printf("  z)oom+ Z-");      break;
-            case 26:    printf("  s)cale+ S-");     break;
-            case 27:    printf("  q)quit");         break;
+            case 12:    printf("  SCROLL");                                        break;
+            case 13:    printf("    "  ANSI_HIGHLIGHT "i" ANSI_NORMAL);            break;
+            case 14:    printf("  "    ANSI_HIGHLIGHT "j" ANSI_NORMAL " + " ANSI_HIGHLIGHT "k" ANSI_NORMAL);    break;
+            case 15:    printf("    "  ANSI_HIGHLIGHT "m" ANSI_NORMAL);            break;
+            case 17:    printf("  PAGE");                                          break;
+            case 18:    printf("    "  ANSI_HIGHLIGHT "I" ANSI_NORMAL);            break;
+            case 19:    printf("  "    ANSI_HIGHLIGHT "J" ANSI_NORMAL " + " ANSI_HIGHLIGHT "K" ANSI_NORMAL);      break;
+            case 20:    printf("    "  ANSI_HIGHLIGHT "M" ANSI_NORMAL);            break;
+            case 22:    printf("  "    ANSI_HIGHLIGHT "c" ANSI_NORMAL "ap switch");                                    break;
+            case 23:    printf("  "    ANSI_HIGHLIGHT "z" ANSI_NORMAL "oom+");     break;
+            case 24:    printf("  "    ANSI_HIGHLIGHT "Z" ANSI_NORMAL "oom-");     break;
+            case 25:    printf("  "    ANSI_HIGHLIGHT "s" ANSI_NORMAL "cale+");    break;                                        break;
+            case 26:    printf("  "    ANSI_HIGHLIGHT "s" ANSI_NORMAL "cale-");    break;                                        break;
+            case 27:    printf("  "    ANSI_HIGHLIGHT "q" ANSI_NORMAL "uit");      break;
             }
             printf("\n");
         }
@@ -254,9 +278,6 @@ void viewLCTuneMap(int tuneChoice, double freq, complex double Zin)
         switch (ch) {
             case 'c':
                 capsw = !capsw;
-                break;
-            case 'h':
-                hit = !hit;
                 break;
             case 'i': 
                 yoff -= zoom;
@@ -327,7 +348,7 @@ void viewResistReactMaps()
         double curScale = scale[sinx];
         for (int i=0; i<sizeof(histogram)/sizeof(histogram[0]); i++)
             histogram[i] = 0;
-        double cumulative = 0.0;
+        double cumulative = 100.0;
 
         for (int r=0; r<N_RESISTANCES; r++) {
             for (int j=0; j<N_REACTANCES; j++) {
@@ -378,7 +399,11 @@ void viewResistReactMaps()
                 for (int j=0; j<N_REACTANCES; j++) {
                     char s[10];
                     sprintf(s, "%d  ", abs((int)j));
+                    if (j%2)
+                        printf(ANSI_HIGHLIGHT);
                     printf("%c", s[n]);
+                    if (j%2)
+                        printf(ANSI_NORMAL);
                 }
                 printf("\n");
             }
@@ -399,7 +424,11 @@ void viewResistReactMaps()
                 printf("%3.0f  ", resistances[r]);
             for (int j=0; j<N_REACTANCES; j++) {
                 int n = digitMap[r][j];
+                if (zoom && j%2)
+                    printf(ANSI_HIGHLIGHT);
                 printf("%c", (n < 0) ? '-' : ((n < 10) ? ('0' + n) : '+'));
+                if (zoom && j%2)
+                    printf(ANSI_NORMAL);
             }
 
             // show the digits legend and histogram on the right of the digit map
@@ -415,19 +444,19 @@ void viewResistReactMaps()
 
             switch (r)
             {
-                case 13:   printf("    f)frequency+");  break;
-                case 14:   printf("    F)frequency-");  break;
-                case 15:   printf("    g)graph+");      break;
-                case 16:   printf("    G)graph+");      break;
-                case 17:   printf("    s)cale+");       break;
-                case 18:   printf("    S)cale-");       break;
-                case 19:   printf("    1)tune");        break;
-                case 20:   printf("    2)tune");        break;
+                case 13:   printf("    " ANSI_HIGHLIGHT "f" ANSI_NORMAL "requency+");  break;
+                case 14:   printf("    " ANSI_HIGHLIGHT "F" ANSI_NORMAL "requency-");  break;
+                case 15:   printf("    " ANSI_HIGHLIGHT "g" ANSI_NORMAL "raph+");      break;
+                case 16:   printf("    " ANSI_HIGHLIGHT "G" ANSI_NORMAL "raph+");      break;
+                case 17:   printf("    " ANSI_HIGHLIGHT "s" ANSI_NORMAL "cale+");       break;
+                case 18:   printf("    " ANSI_HIGHLIGHT "S" ANSI_NORMAL "cale-");       break;
+                case 19:   printf("    " ANSI_HIGHLIGHT "1" ANSI_NORMAL "tune");        break;
+                case 20:   printf("    " ANSI_HIGHLIGHT "2" ANSI_NORMAL "tune");        break;
             }
 
             // show the digits legend and histogram on the right of the digit map
             double percent = 100.0*histogram[r]/(N_RESISTANCES*N_REACTANCES);
-            cumulative += percent;
+            cumulative -= percent;
             if (r <= 11)
                 printf(" %5.1f%%  %5.1f%%\n", percent, cumulative);
             else           
@@ -508,22 +537,20 @@ int main()
             for (int j=0; j<N_REACTANCES; j++) 
                 bestSWRs[f][r][j]  = exaustiveSearch(freqs[f], resistances[r]+I*reactances[j]);
 
-   for (tuneFreqInx=0; tuneFreqInx<N_FREQS; tuneFreqInx++) {
+   for (int i=0; i<N_FREQS; i++) {
         for (int r=0; r<N_RESISTANCES; r++) {
             for (int j=0; j<N_REACTANCES; j++) {
-                tuneImp = resistances[r]+I*reactances[j];
-
-                tuneCount = 0;
+                resetTune(freqs[i], resistances[r]+I*reactances[j]);
                 extern void tune1();
                 tune1(); 
-                tunedSWRs[0][tuneFreqInx][r][j] = SWRexact;
-                tuneCounts[0][tuneFreqInx][r][j] = tuneCount;
+                tunedSWRs[0][i][r][j] = SWRexact;
+                tuneCounts[0][i][r][j] = tuneCount;
 
-                tuneCount = 0;
+                resetTune(freqs[i], resistances[r]+I*reactances[j]);
                 extern void tune2();
                 tune2(); 
-                tunedSWRs[1][tuneFreqInx][r][j] = SWRexact;
-                tuneCounts[1][tuneFreqInx][r][j] = tuneCount;
+                tunedSWRs[1][i][r][j] = SWRexact;
+                tuneCounts[1][i][r][j] = tuneCount;
             }
         }
     }
